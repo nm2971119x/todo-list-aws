760fdc2f7c2bfec8c2f4db8915fa0904d285115f
Jarun Delgado <jarun.delgado673@comunidadunir.net>
2022-01-29 01:12:50 +0000

    review

---
+++ pipelines/PIPELINE-FULL-REVIEW/unit_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source todo-list-aws/bin/activate
+set -x
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+echo "PYTHONPATH: $PYTHONPATH"
+export DYNAMODB_TABLE=todoUnitTestsTable
+python test/unit/TestToDo.py
+pip show coverage
+coverage run --include=src/todoList.py test/unit/TestToDo.py
+coverage report
+coverage xml
+coverage html
+tar vfcz /var/lib/jenkins/workspace/PIPELINE-FULL-STAGING/htmlcov.tgz /var/lib/jenkins/workspace/PIPELINE-FULL-STAGING/htmlcov/
+
